fdf914219eed24cd8a287229dcd0589929add6f3
kahturibio <104437741+kahturibio@users.noreply.github.com>
2024-03-12 19:25:09 -0300

    formatação

--- formadores/main.js
+++ formadores/main.js
@@ -3,7 +3,7 @@ const botoes = document.querySelectorAll(".botao");
 // iterando sobre botões
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for (let j = 0; < botoes.length; j++){
+        for (let j = 0; < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
