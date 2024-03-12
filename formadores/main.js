3a4c7068a809574babc209bce7c13414b6d2814d
kahturibio <104437741+kahturibio@users.noreply.github.com>
2024-03-12 19:22:19 -0300

    ..

--- formadores/main.js
+++ formadores/main.js
@@ -1,7 +1,12 @@
-const botoes = document. querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
 
 // iterando sobre botões
-for(let i = 0; i < botoes.length; i++) {
-    botoes [i].onclick
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo")
+    }
 
 }
