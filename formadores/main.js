0aa4435e63ec20ad5217c2a467ba74267585821d
kahturibio <104437741+kahturibio@users.noreply.github.com>
2024-03-20 18:24:21 -0300

    Update main.js

--- formadores/main.js
+++ formadores/main.js
@@ -1,12 +1,23 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-// iterando sobre botões
-for (let i = 0; i < botoes.length; i++) {
+//iterando (percorrer elementos de uma lista) sobre a lista de botoes 
+for (let i=0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for (let j = 0; < botoes.length; j++) {
+        for (let j=0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
-    }
+        textos[i].classList.add("ativo");
 
+    }
 }
+
+//document.querySelector("#minhaDiv p").textContent = "Olá Chuva!";
+const contadores = document.querySelectorAll (".contador");
+
+const tempoAtual = new Date(); //retorna data atual do computador
+const tempoObjetivo1 = new Date("2024-10-13T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual; 
